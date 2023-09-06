ebde827b092c64c5761eb0e0c84d5b8fb9b59edf
MilenaJ-Solicy <milena.janoyan@solicy.net>
2023-09-06 18:58:23 +0400

    Added example of one to one relation

--- models/user.js
+++ models/user.js
@@ -1,20 +1,24 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      // define association here
+      User.hasOne(models.UserProfile, { foreignKey: 'userId', as: 'profile' });
     }
   }
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+
+  User.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+
   return User;
 };
