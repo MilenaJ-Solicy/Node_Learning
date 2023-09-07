5919b03d4ed44be8b3b76eb6523869f495b04ea6
MilenaJ-Solicy <milena.janoyan@solicy.net>
2023-09-07 18:12:54 +0400

    Added many-to-many relation example

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       User.hasOne(models.UserProfile, { foreignKey: 'userId', as: 'profile' });
+      User.belongsToMany(models.Interest, { through: models.UserInterest, foreignKey: 'userId' });
     }
   }
 
